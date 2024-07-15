71967892b50b0bafaabdaa6f4a5ce04dd19ad00d
Snow Yáng <snow.yang@fluv.com>
2024-07-15 21:07:09 +0800

    Initial Commit

---
+++ ios-weather/ios_weatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_weatherApp.swift
+//  ios-weather
+//
+//  Created by S on 2024/7/15.
+//
+
+import SwiftUI
+
+@main
+struct ios_weatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
