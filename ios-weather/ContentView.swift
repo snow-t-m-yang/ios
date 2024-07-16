836cbf401ec5bc5d1527a605fa66fa1e9f1654f0
Snow Yáng <snow.yang@fluv.com>
2024-07-16 21:07:45 +0800

    Add my first swift state

--- ios-weather/ContentView.swift
+++ ios-weather/ContentView.swift
@@ -8,12 +8,11 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var status = "night"
+    
     var body: some View {
         ZStack {
-            LinearGradient(gradient:Gradient(colors: [Color("Primary"), Color("Secondary")]),
-                           startPoint: .topLeading,
-                           endPoint: .bottomTrailing)
-            .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            BGView(top: status == "night" ? Color("PrimaryBG") : .indigo, bottom: status == "night" ? Color("SecondaryBG") : .teal)
             
             VStack(spacing: 9){
                 Text("Sangenjaya")
@@ -46,8 +45,21 @@ struct ContentView: View {
                                    image: "snow",
                                    tem: 0)
                     
-                }
+                }.padding(30)
                 
+                Button {
+					if status == "night" {
+						status = "day"
+					} else {
+						status = "night"
+					}
+                } label: {
+                    Text("Change the Theme!")
+                        .frame(width: 300, height: 50)
+                        .background(Color.white)
+                        .font(.system(size: 20, weight: .bold, design: .monospaced))
+                        .cornerRadius(30)
+                }
             }
         }
     }
@@ -78,5 +90,17 @@ struct WeatherDayView: View {
                     .font(.system(size: 33, weight: .semibold, design: .default))
             }
         }
+    }
+}
+
+struct BGView: View {
+    var top: Color
+    var bottom: Color
+    
+    var body: some View {
+        LinearGradient(gradient:Gradient(colors: [top, bottom]),
+                       startPoint: .topLeading,
+                       endPoint: .bottomTrailing)
+		.ignoresSafeArea(.all)
     }
 }
