9e66a550116dd2b1a0911458d3b88d4cc0d1797d
Snow Yáng <snow.yang@fluv.com>
2024-07-24 20:34:51 +0800

    Add views

---
+++ ios-weather/Appetizer/Appetizer.swift
@@ -0,0 +1,38 @@
+//
+//  Appetizer.swift
+//  ios-weather
+//
+//  Created by S on 2024/7/24.
+//
+
+import Foundation
+
+struct Appetizer: Decodable, Identifiable {
+	let id: Int
+	let name: String
+	let description: String
+	let price: Double
+	let imageURL: String
+	let calories: Int
+	let protein: Int
+	let carbs: Int
+}
+
+struct AppetizerResponse {
+	let request: [Appetizer]
+}
+
+struct AMockData {
+	static let sampleAppetizer = Appetizer(
+		id: 0001,
+		name: "test",
+		description: "tesing",
+		price: 777,
+		imageURL: "",
+		calories: 10,
+		protein: 35,
+		carbs: 0
+	)
+
+	static let appetizers = [sampleAppetizer, sampleAppetizer, sampleAppetizer, sampleAppetizer]
+}
--- ios-weather/Appetizer/AppetizerListView.swift
+++ ios-weather/Appetizer/AppetizerListView.swift
@@ -10,8 +10,26 @@ import SwiftUI
 struct AppetizerListView: View {
 	var body: some View {
 		NavigationView {
-			Text("list")
-				.navigationTitle("Appetizers")
+			List(AMockData.appetizers) { app in
+				HStack {
+					Image("sf-symbols")
+						.resizable()
+						.aspectRatio(contentMode: .fit)
+						.frame(width: 120, height: 90)
+						.cornerRadius(9)
+
+					VStack(alignment: .leading, spacing: 5) {
+						Text(app.name)
+							.font(.title2)
+							.fontWeight(.medium)
+						Text("$\(app.price, specifier: "%.2f")")
+							.foregroundColor(.secondary)
+							.fontWeight(.semibold)
+					}
+					.padding(.leading)
+				}
+			}
+			.navigationTitle("🍣 Appetizers")
 		}
 	}
 }
